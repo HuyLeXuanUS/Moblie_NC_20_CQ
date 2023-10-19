27ee588a849dab5be86c73e5d8cbaeb805b9d2bf
HuyLeXuanUS <xuanhuy.tqn.005@gmail.com>
2023-10-19 23:40:41 +0700

    Update man hinh danh sach giao vien

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:date_field/date_field.dart';
+//import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() {runApp(MyApp());}
 
 class MyApp extends StatelessWidget {
   @override
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// Màn hình chuyển hướng body
+// Màn hình chuyển hướng body của scarffold
 class MyHomePage extends StatefulWidget {
   @override
   _MyHomeState createState() => _MyHomeState();
@@ -23,7 +23,6 @@ class _MyHomeState extends State<MyHomePage> {
   Widget _currentBody = ListTeacherPage();
 
   String activeButton = '';
-
   String activingButton = 'GIA SƯ';
 
   String selectedLanguage = 'Tiếng Việt';
@@ -610,23 +609,8 @@ class ListTeacherPage extends StatefulWidget {
 }
 
 class _ListTeacherState extends State<ListTeacherPage>{
-  String selectedLanguage = 'English';
-
-  String activeButton = 'GIA SƯ';
-
-  String setBlueButton = 'GIA SƯ';
-
-  void setActiveButton(String button) {
-    setState(() {
-      activeButton = button;
-    });
-  }
-
-  void resetButtonsColor() {
-    setState(() {
-      activeButton = 'GIA SƯ';
-    });
-  }
+  //final TextEditingController _dateController = TextEditingController();
+  DateTime selectedDate = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
@@ -683,26 +667,46 @@ class _ListTeacherState extends State<ListTeacherPage>{
               ),
             ),
             Container(
-              padding: EdgeInsets.all(16.0),
+              padding: EdgeInsets.only(
+                top: 30.0, // Padding ở phía trên
+                left: 80.0, // Padding ở phía bên trái
+              ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text('Hàng 1'), // Hàng 1: Text
-                  // Hàng 2: Hai TextField và DropdownButton
+                  Text(
+                    'Tìm kiếm gia sư',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold, // In đậm
+                      color: Colors.black, // Màu đen
+                      fontSize: 30, // Cỡ chữ 30
+                    ),
+                  ),
+                  SizedBox(height: 30),
                   Row(
-                    children: <Widget>[
-                      Expanded(
+                    children: <Widget>[                       
+                      Container(
+                        width: 300.0,
+                        padding: EdgeInsets.only(left: 30), // Padding cho TextField
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 1.0), // Viền
+                          borderRadius: BorderRadius.circular(60.0), // Góc bo tròn
+                        ),
                         child: TextField(
                           decoration: InputDecoration(
-                            labelText: 'Tên gia sư',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
+                            labelText: 'Nhập tên gia sư..',
+                            border: InputBorder.none, // Loại bỏ viền mặc định
                           ),
                         ),
                       ),
-                      SizedBox(width: 16.0), // Khoảng cách giữa các phần tử
-                      Expanded(
+                      SizedBox(width: 16.0),
+                      Container(
+                        width: 600.0,
+                        padding: EdgeInsets.only(left: 30, right: 30), // Padding cho DropdownButton
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 1.0), // Viền
+                          borderRadius: BorderRadius.circular(60.0), // Góc bo tròn
+                        ),
                         child: DropdownButton<String>(
                           value: 'Gia sư nước ngoài',
                           onChanged: (String? newValue) {
@@ -722,18 +726,35 @@ class _ListTeacherState extends State<ListTeacherPage>{
                       ),
                     ],
                   ),
-                  Text('Hàng 3'), // Hàng 3: Text
+                  SizedBox(height: 16.0),
+                  Text(
+                    'Chọn thời gian dạy kèm có lịch trống: ',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold, // In đậm
+                      color: Colors.black, // Màu đen
+                      fontSize: 18, // Cỡ chữ 18
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
                   // Hàng 4: Hai TextField và DropdownButton
                   Row(
                     children: <Widget>[
                       Expanded(
-                        child: TextField(
+                        child: DateTimeField(
+                          selectedDate: DateTime.now(), // Đặt giá trị ngày và giờ mặc định
                           decoration: InputDecoration(
-                            labelText: 'Ngày',
+                            hintStyle: TextStyle(color: Colors.black45),
+                            errorStyle: TextStyle(color: Colors.redAccent),
                             border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0),
+                              borderRadius: BorderRadius.circular(50.0), // Đặt bo tròn
                             ),
+                            suffixIcon: Icon(Icons.event_note),
+                            labelText: 'Chọn ngày..', // Đổi tên hiển thị
                           ),
+                          mode: DateTimeFieldPickerMode.date, // Chọn cả ngày và giờ
+                          onDateSelected: (DateTime value) {
+                            //print(value.day.toString()+'-'+value.month.toString()+'-'+value.year.toString());
+                          },
                         ),
                       ),
                       SizedBox(width: 16.0),
@@ -766,26 +787,201 @@ class _ListTeacherState extends State<ListTeacherPage>{
                       ),
                     ],
                   ),
-                  // Hàng 5: 12 button lọc
-                  Wrap(
-                    spacing: 8.0, // Khoảng cách giữa các button
-                    children: List.generate(12, (index) {
-                      return ElevatedButton(
-                        onPressed: () {
-                          // Xử lý sự kiện khi nút được nhấn
-                        },
-                        child: Text('Lọc $index'),
-                      );
-                    }),
+                  SizedBox(height: 30.0),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 200),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        ElevatedButton(
+                          onPressed: () {
+                            // Xử lý sự kiện khi nút được nhấn
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color.fromARGB(255, 219, 216, 216), // Đặt màu nền xám
+                            onPrimary: Colors.black, // Đặt màu chữ đen
+                            elevation: 0, // Loại bỏ độ sâu
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0), // Đặt bo tròn
+                            ),
+                          ),
+                          child: Text('Tất cả'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Xử lý sự kiện khi nút được nhấn
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color.fromARGB(255, 219, 216, 216), // Đặt màu nền xám
+                            onPrimary: Colors.black, // Đặt màu chữ đen
+                            elevation: 0, // Loại bỏ độ sâu
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0), // Đặt bo tròn
+                            ),
+                          ),
+                          child: Text('Tiếng Anh cho trẻ em'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Xử lý sự kiện khi nút được nhấn
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color.fromARGB(255, 219, 216, 216), // Đặt màu nền xám
+                            onPrimary: Colors.black, // Đặt màu chữ đen
+                            elevation: 0, // Loại bỏ độ sâu
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0), // Đặt bo tròn
+                            ),
+                          ),
+                          child: Text('Tiếng Anh cho công việc'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Xử lý sự kiện khi nút được nhấn
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color.fromARGB(255, 219, 216, 216), // Đặt màu nền xám
+                            onPrimary: Colors.black, // Đặt màu chữ đen
+                            elevation: 0, // Loại bỏ độ sâu
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0), // Đặt bo tròn
+                            ),
+                          ),
+                          child: Text('Giao tiếp'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Xử lý sự kiện khi nút được nhấn
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color.fromARGB(255, 219, 216, 216), // Đặt màu nền xám
+                            onPrimary: Colors.black, // Đặt màu chữ đen
+                            elevation: 0, // Loại bỏ độ sâu
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0), // Đặt bo tròn
+                            ),
+                          ),
+                          child: Text('STARTERS'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Xử lý sự kiện khi nút được nhấn
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color.fromARGB(255, 219, 216, 216), // Đặt màu nền xám
+                            onPrimary: Colors.black, // Đặt màu chữ đen
+                            elevation: 0, // Loại bỏ độ sâu
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0), // Đặt bo tròn
+                            ),
+                          ),
+                          child: Text('MOVERS'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Xử lý sự kiện khi nút được nhấn
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color.fromARGB(255, 219, 216, 216), // Đặt màu nền xám
+                            onPrimary: Colors.black, // Đặt màu chữ đen
+                            elevation: 0, // Loại bỏ độ sâu
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0), // Đặt bo tròn
+                            ),
+                          ),
+                          child: Text('FLYERS'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Xử lý sự kiện khi nút được nhấn
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color.fromARGB(255, 219, 216, 216), // Đặt màu nền xám
+                            onPrimary: Colors.black, // Đặt màu chữ đen
+                            elevation: 0, // Loại bỏ độ sâu
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0), // Đặt bo tròn
+                            ),
+                          ),
+                          child: Text('KET'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Xử lý sự kiện khi nút được nhấn
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color.fromARGB(255, 219, 216, 216), // Đặt màu nền xám
+                            onPrimary: Colors.black, // Đặt màu chữ đen
+                            elevation: 0, // Loại bỏ độ sâu
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0), // Đặt bo tròn
+                            ),
+                          ),
+                          child: Text('PET'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Xử lý sự kiện khi nút được nhấn
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color.fromARGB(255, 219, 216, 216), // Đặt màu nền xám
+                            onPrimary: Colors.black, // Đặt màu chữ đen
+                            elevation: 0, // Loại bỏ độ sâu
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0), // Đặt bo tròn
+                            ),
+                          ),
+                          child: Text('IELTS'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Xử lý sự kiện khi nút được nhấn
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color.fromARGB(255, 219, 216, 216), // Đặt màu nền xám
+                            onPrimary: Colors.black, // Đặt màu chữ đen
+                            elevation: 0, // Loại bỏ độ sâu
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0), // Đặt bo tròn
+                            ),
+                          ),
+                          child: Text('TOEFL'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Xử lý sự kiện khi nút được nhấn
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color.fromARGB(255, 219, 216, 216), // Đặt màu nền xám
+                            onPrimary: Colors.black, // Đặt màu chữ đen
+                            elevation: 0, // Loại bỏ độ sâu
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0), // Đặt bo tròn
+                            ),
+                          ),
+                          child: Text('TOEIC'),
+                        ),
+                        // Thêm các button khác tương tự cho các mục tiêu còn lại
+                      ],
+                    ),
                   ),
                   // Hàng 6: Nút đặt lại bộ tìm kiếm
+                  SizedBox(height: 30.0),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       ElevatedButton(
                         onPressed: () {
                           // Xử lý sự kiện khi nút được nhấn
                         },
+                        style: ElevatedButton.styleFrom(
+                          primary: Color.fromARGB(255, 169, 184, 224), // Đặt màu nền xám
+                          onPrimary: Colors.black, // Đặt màu chữ đen
+                          elevation: 0, // Loại bỏ độ sâu
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0), // Đặt bo tròn
+                          ),
+                        ),
                         child: Text('Đặt lại bộ tìm kiếm'),
                       ),
                     ],
@@ -798,7 +994,37 @@ class _ListTeacherState extends State<ListTeacherPage>{
               thickness: 1.0, // Độ dày của đường kẻ
             ),
             Container(
-              
+              padding: EdgeInsets.only(top:30.0, left: 80.0),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Text(
+                    'Gia sư được đề xuất',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold, // In đậm
+                      color: Colors.black, // Màu đen
+                      fontSize: 30, // Cỡ chữ 30
+                    ),
+                  ),
+                    ],
+                  ),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: , // Số lượng hàng bạn muốn hiển thị
+                    itemBuilder: (BuildContext context, int index) {
+                      // Tạo một dòng gồm 3 View
+                      return Row(
+                        children: <Widget>[
+                          // Đặt View 1 ở đây
+                          // Đặt View 2 ở đây
+                          // Đặt View 3 ở đây
+                        ],
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
             Container(
               decoration: BoxDecoration(
@@ -906,42 +1132,6 @@ class _ListTeacherState extends State<ListTeacherPage>{
             ),
           ],
         )
-      ),
-    );
-  }
-
-  void _handleLanguageSelection(String value) {
-    setState(() {
-      selectedLanguage = value;
-    });
-  }
-
-  Widget _buildAppBarButton(String text) {
-    bool isActive = text == activeButton;
-
-    if(text == setBlueButton){
-      isActive = true;
-    }
-
-    return GestureDetector(
-      onTap: () {
-        setActiveButton(text);
-      },
-      child: MouseRegion(
-        onEnter: (event) {
-          setActiveButton(text);
-        },
-        onExit: (event) {
-          resetButtonsColor();
-        },
-        child: Text(
-          text,
-          style: TextStyle(
-            fontSize: 18,
-            color: isActive ? Color.fromARGB(255, 3, 117, 210) : Colors.grey,
-            fontWeight: isActive ? FontWeight.bold : FontWeight.normal,
-          ),
-        ),
       ),
     );
   }
